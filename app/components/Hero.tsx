362a5096a49a333115bb71e06315ec168d1f6850
Senpai <clevercoder0307@gmail.com>
2025-11-06 01:01:23 +0530

    landing page done

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,19 @@
 import Image from 'next/image'
-import React from 'react'
+import Link from 'next/link'
 
 const Hero = () => {
   return (
     <div className="h-dvh w-full">
-      <div className="relative h-full w-full">
-        <Image src={"https://images.pexels.com/photos/13731277/pexels-photo-13731277.jpeg"} alt="Real Standard of Living" className="" fill/>
+      <div className="relative h-full w-full bg-black">
+        <Image src={"https://images.pexels.com/photos/7601262/pexels-photo-7601262.jpeg"} alt="Real Standard of Living" className="object-cover opacity-40" fill/>
+      </div>
+      <div className='absolute top-0 left-0 pt-15 h-full w-full flex flex-col gap-5 text-white  items-center justify-center'>
+        <h3 className='italic font-serif text-base'>FInd. Buy. Live. Love.</h3>
+        <h1 className='font-serif text-7xl'>A New Standard of Living</h1>             
+        <p className='w-90 text-center'>Discover an exclusive portfolio of properties across continents - each defined by architecture, artistry and unparalleled service.</p>
+        <Link href="/" className='mt-6 uppercase bg-white text-black font-semibold py-4 px-12 text-sm'>
+            Discover 
+        </Link>
       </div>
     </div>
   )
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,28 @@
+import { MapPin } from "lucide-react"
+import Link from "next/link"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="fixed z-10 w-full px-20 py-5 text-white flex justify-between border-b border-white/50">   
+        <div className="lg:text-xl font-medium font-serif grid grid-cols-2">
+            Briar & Co.
+        </div>
+        <div className="gap-8 grid  grid-cols-5 ">
+            <Link href={"/"}>Buy</Link>
+            <Link href={"/"}>Sell</Link>
+            <Link href={"/"}>Properties</Link>
+            <Link href={"/"}>About us</Link>
+            <Link href={"/"}>Contact</Link>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+            <div className="flex gap-2">
+                <MapPin /> Miami, FL
+            </div>
+            <Link href="/">
+                Client Login
+            </Link>
+        </div>
+    </div>
   )
 }
 
